8a26cbc28272c5561de67c63c8214f7405bd7dae
dvddvd300 <21372070+dvddvd300@users.noreply.github.com>
2026-05-25 23:18:23 -0600

    Update config.py

--- d3_thread_spawner/config.py
+++ d3_thread_spawner/config.py
@@ -4,7 +4,10 @@ from __future__ import annotations
 
 import os
 import sys
-import tomllib
+try:
+    import tomllib
+except ModuleNotFoundError:
+    import tomli as tomllib
 from pathlib import Path
 from typing import Any, Dict, Optional
 
